aa52eaaa97ebd509709ed67465442b5da06fca9b
codexmaker <codexmaker12@gmail.com>
2020-05-08 14:03:12 -0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+
+declare var paypal;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+   
+  @ViewChild('paypal', { static: true }) paypalElement : ElementRef;
+
+  producto = {
+    descripcion : 'producto en venta',
+    precio      : 599.99,
+    img         : 'imagen de tu producto'
+  }
   title = 'angular-paypal-payment';
+
+  ngOnInit(){
+
+    paypal
+    .Buttons({
+      createOrder: (data, actions) => {
+        return actions.order.create({
+          purchase_units: [
+            {
+              description: this.producto.descripcion,
+              amount     :{
+                currency_code: 'MXN',
+                value        : this.producto.precio
+              }
+            }
+          ]
+        })
+      },
+      onApprove: async (data, actions) => {
+        const order = await actions.order.capture();
+        console.log(order);
+        
+      },
+      onError: err =>{
+        console.log(err);
+        
+      }
+    })
+    .render( this.paypalElement.nativeElement );
+
+  }
 }
